807d169c226980b13125e4177e9a9e67d13f8ade
Marty McGee <support@companyjuice.com>
2025-05-22 10:14:33 -0700

    v0.1.2: https://github.com/marty-mcgee/gltf-react-three

--- components/fileDrop.js
+++ components/fileDrop.js
@@ -10,19 +10,20 @@ const FileDrop = ({ onDrop, useSuzanne }) => {
       <input {...getInputProps()} />
 
       {isDragActive ? (
-        <p className="text-4xl font-bold text-blue-600">Drop the files here...</p>
+        <p className="text-4xl font-bold text-blue-600">Drop file here...</p>
       ) : (
         <p className="text-4xl font-bold">
-          Drag {"'"}n{"'"} drop your GLTF file {" "}
+          {/* Drag {"'"}n{"'"}  */}
+          Drop your GLB or GLTF file {" "}
           <button className="text-blue-600">here</button>
-          {" "} or try it with {" "}
+          {" "} or open the demo file {" "}
           <button className="text-blue-600" onClick={useSuzanne}>
-            Suzanne
+            Chimpanzee
           </button>
         </p>
       )}
       {fileRejections.length ? (
-        <p className="block text-center text-xl pt-4 text-red-300">Only .gltf or .glb files are accepted</p>
+        <p className="block text-center text-xl pt-4 text-red-300">Only .glb or .gltf files are accepted</p>
       ) : null}
     </div>
   )
