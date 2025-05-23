bea2a3fa7b9f80662c7bace64815730c18f49134
Marty McGee <support@companyjuice.com>
2025-05-22 22:05:27 -0700

    v0.1.3: https://github.com/marty-mcgee/gltf-react-three

--- components/footer.js
+++ components/footer.js
@@ -5,34 +5,47 @@ import {
 // import { Logo } from 'threed-garden'
 
 const Footer = () => (
-  <footer className="p-4 flex items-center justify-between w-full bg-darkgray z-10 relative">
-    <a href="https://threedgarden.com/#gltfjsx" target="_blank" rel="noreferrer">
+  <footer className="p-2 flex items-center justify-between w-full bg-darkgray z-10 relative text-xs text-gray-50">
+    <a 
+      href="https://threedgarden.com/#gltfjsx" 
+      target="_blank" 
+      rel="noreferrer"
+    >
       {/* <Logo /> */}
       🥕 ThreeD Garden: GLB-to-TSX Converter
     </a>
-    <p className="text-xs">
+    <a 
+      href="https://github.com/marty-mcgee/gltf-react-three/"
+      target="_blank" 
+      rel="noreferrer"
+    >
+      <GitHub />
+    </a>
+    <p className="text-s">
       Made by{' '}
       <a
         className="underline hover:text-blue-600"
         href="https://github.com/marty-mcgee/threed-garden"
         target="_blank"
-        rel="noreferrer">
+        rel="noreferrer"
+      >
         @marty-mcgee
       </a>{' '}
-      &{' '}
+      +{' '}
       <a
         className="underline hover:text-blue-600"
-        href="https://github.com/marty-mcgee/gltf-react-three/graphs/contributors">
+        href="https://github.com/marty-mcgee/gltf-react-three/graphs/contributors"
+      >
         contributors
       </a>{' '}
-      inspired by the{' '}
-      <a className="underline hover:text-blue-600" href="https://github.com/pmndrs/gltfjsx">
+      inspired by{' '}
+      <a 
+        className="underline hover:text-blue-600" 
+        href="https://github.com/pmndrs/gltfjsx"
+      >
         gltfjsx cli
       </a>
     </p>
-    <a href="https://github.com/marty-mcgee/gltf-react-three/" target="_blank" rel="noreferrer">
-      <GitHub />
-    </a>
   </footer>
 )
 
